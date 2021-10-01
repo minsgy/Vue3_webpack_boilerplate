33a0495dfbc4eb03393cc415fd7bca4265af2d7f
minsgy <alstjrchl97@naver.com>
2021-10-01 16:41:36 +0900

    [feat] 양방향 커스텀 이벤트 통신

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <h1>
+    {{ name }}
+  </h1>
+  <h1>
     {{ msg }}
   </h1>
-  <Hello />
+  <!-- :message와 같이 명시해주어서 예약어를 사용 하지 않아도 됩니다. -->
+  <!-- 부모에 양방향 데이터를 설정해서 통신이 가능하다 -->
+  <Hello
+    v-model:message="msg"
+    v-model:name="name" />
 </template>
 <script>
 import Hello from '~/components/Hello'
@@ -14,6 +21,7 @@ export default {
   data() {
     return {
       msg: 'Hello Vue...',
+      name: '민석'
     }
   },
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,34 @@
 <template>
-  <h1>ㅎㅇㅋㅋ</h1>
+  <label>
+    <!-- v-model 양방향 시 props로 인해 오류가 발생합니다. (바뀌지도 않음) -->
+    <!-- emit을 통해서 자식에서 부모로 올려주는 방식을 구현합니다. -->
+    <input 
+      :value="message"
+      @input="$emit('update:message', $event.target.value)" />
+  </label>
+  <label>
+    <input
+      :value="name"
+      @input="$emit('update:name', $event.target.value)" />
+  </label>
 </template>
+
+<script>
+  export default {
+    props: {
+      // v-model과 연결 된 "예약어" => model 값이 여기로 들어옴. modelValue
+      // 이름 변경도 가능합니다. 대신 다른 처리가 필요합니다.
+      message: {
+        type: String,
+        default: ''
+      },
+      name : {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
 
 <style scoped lang="scss">
 $color: red;
