7277266c61dc159829bb21baf45359aee2032178
minsgy <alstjrchl97@naver.com>
2021-10-01 18:59:48 +0900

    [feat] nextTick과 비동기 작업

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <h1>
+  <div v-if="!isEdit">
     {{ msg }}
-  </h1>
-  <Hello />
+    <button @click="onEdit">
+      Edit!
+    </button>
+  </div>
+  <div v-else>
+    <input
+      ref="editor"
+      v-model="msg"
+      @keyup.enter="isEdit=false" />
+  </div>
 </template>
 <script>
-import Hello from '~/components/Hello'
 
 export default {
-  components: {
-    Hello,
-  },
   data() {
     return {
+      isEdit: false,
       msg: 'Hello Vue...',
     }
   },
+  methods: {
+    onEdit(){
+      this.isEdit = true
+      // 버튼을 클릭하자마자 포커싱이 되도록 설정!
+      // DOM 업데이트 주기 이후 실행 될 콜백을 연기합니다. refs랑 같이 사용
+      this.$nextTick(() => {
+        this.$refs.editor.focus()
+      })
+    }
+  }
 }
 </script>
