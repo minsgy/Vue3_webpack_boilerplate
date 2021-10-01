4fdd8097797907336d2bcd1e412bda4ade519dcc
minsgy <alstjrchl97@naver.com>
2021-10-01 17:04:34 +0900

    [feat] 동적으로 slot name 관리하기

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <h1>
+  <h1
+    @click="slotName = 'abc'">
     {{ msg }}
   </h1>
-  <Hello />
+  <Hello>
+    <!-- default 기본 슬롯 => 값을 끌어와 사용합니다.-->
+    <!-- 범위를 갖는 슬롯 - default만 사용가능하다~ -->
+    <template
+      #default="{
+        hello
+      }">
+      <h2>
+        Hello {{ hello }}
+      </h2>
+    </template>
+    <!-- 여기선 확장 불가임. -->
+    <template #[slotName]>
+      <h2>
+        ABC
+      </h2>
+    </template>
+  </Hello>
 </template>
 <script>
 import Hello from '~/components/Hello'
@@ -14,6 +32,7 @@ export default {
   data() {
     return {
       msg: 'Hello Vue...',
+      slotName: 'xyz'
     }
   },
 }
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>ㅎㅇㅋㅋ</h1>
+  <!-- 객체 데이터로 랩핑되서 넘어와서 사용 할 수 있습니다. {} 로도 사용가능 -->
+  <slot :hello="123"></slot>
+  <slot name="abc"></slot>
 </template>
 
 <style scoped lang="scss">
