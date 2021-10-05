cb91040cb957736cf30bfc35b8e9e537e23b0d9d
minsgy <alstjrchl97@naver.com>
2021-10-05 20:58:43 +0900

    feat : babel setting

---
+++ main.js
@@ -0,0 +1,7 @@
+const d = {}
+const c = d.name
+
+const increase = num => num + 1
+
+const arr = []
+console.log(arr.includes('a'))
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import * as Vue from 'vue'
-import App from './App'
-
-Vue.createApp(App).mount('#app')
