5017153b608fdf528b11068487305a569a163168
minsgy <alstjrchl97@naver.com>
2021-10-05 21:38:06 +0900

    feat: vue-router setting

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <Hello />
+  <RouterView />
 </template>
 <script>
-import Hello from '~/components/Hello'
 
 export default {
-  components: {
-    Hello,
-  },
-  data() {
-    return {
-      msg: 'Hello Vue...',
-    }
-  },
+
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {createApp} from 'vue'
 import App from '~/App'
 import store from '~/store'
+import router from '~/routes'
 
 const app = createApp(App)
 
+app.use(router)
 app.use(store)
 app.mount('#app')
